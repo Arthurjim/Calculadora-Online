faee81f425592b91d9f9939baa7d9e19389b4445
Arthur Jimenez <arthurjim013@gmail.com>
2020-11-05 15:59:18 -0600

    Terminado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const number = document.getElementById('teclas');
 const input = document.getElementById("input")
+let OperationStatus = false;
+let number1, typeOperation
 input.textContent = '0';
 const calculador = () => {
   if(!number) return
@@ -13,7 +15,7 @@ const calculador = () => {
         obtenerOperador(t,d.opera)
       }
       if(d.result){
-        correrOperacion()
+        correrOperacion(d.result)
       }
       
 
@@ -21,10 +23,55 @@ const calculador = () => {
 }
 
 const mostrarNumero = number =>{
-  input.textContent === '0' ? input.textContent = number : input.textContent += number
-  
+  input.textContent === '0' || OperationStatus === true 
+    ? input.textContent = number 
+    : number === "." && !input.textContent.includes('.')
+     ? input.textContent += number
+     :number !== '.'
+      ?input.textContent += number
+      :null
+  OperationStatus = false;
 }
 const obtenerOperador =(elemento, operador)=>{
+  OperationStatus =true;
+  number1 = Number(input.textContent)
+  typeOperation = operador
   input.textContent = elemento.textContent
+  
+}
+const correrOperacion = (operation)=>{
+  
+  const getResult = (number1, typeOperation) => {
+    const number2 = Number(input.textContent)  ;
+    var result;
+    switch (typeOperation) {
+      case 'sumar':
+        result = number1 + number2
+        break;
+      case 'restar':
+        result = number1 - number2
+        break;
+      case 'multiplicar':
+        result = number1 * number2
+        break;
+      case 'dividir':
+        result = number1 / number2
+        break;
+      default:
+        result = '0'
+        break;
+    }
+    if(input.textContent === 'NaN'){
+      input.textContent = "Error"
+    }
+    result === Infinity
+      ? input.textContent = "Error"
+      : input.textContent = result;
+  }
+  operation === 'clear'
+    ? input.textContent = "0"
+    : getResult(number1, typeOperation)
+  
+  OperationStatus = true;
 }
 calculador();
